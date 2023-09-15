711e94b68ca394a1555ab8276d0672e8b97880c6
Reagan-prog <reaganjuma1111@gmail.com>
2023-09-15 07:20:24 -0700

    Multiple mains

---
+++ 0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mein - tests function of positive and or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative (i);
+
+	return (0);
+}
